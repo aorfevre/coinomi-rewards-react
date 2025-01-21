aacd187d0c9fe3df7d36795a8d53e0738b55f86a
alex <aorfevre@gmail.com>
2025-01-21 09:21:01 +0100

    fix build

--- functions/src/referrals.ts
+++ functions/src/referrals.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import { db } from './config/firebase';
-import { Timestamp } from 'firebase-admin/firestore';
 
 // Generate a random 7-character code
 const generateReferralCode = (): string => {
