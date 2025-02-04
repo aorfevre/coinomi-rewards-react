7084482c81593905b89b4869a6c0db339b5f7c87
alex <aorfevre@gmail.com>
2025-02-04 20:34:34 +0100

    fix build

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Mock firebase modules
-jest.mock('firebase/app');
-jest.mock('firebase/auth');
-jest.mock('firebase/firestore');
-jest.mock('firebase/functions');
+// Mock Firebase functions
+jest.mock('firebase/functions', () => ({
+    httpsCallable: jest.fn(() => Promise.resolve({ data: {} })),
+    getFunctions: jest.fn(),
+}));
+
+// Mock Firebase auth
+jest.mock('firebase/auth', () => ({
+    signInWithCustomToken: jest.fn(),
+    getAuth: jest.fn(() => ({
+        currentUser: null,
+    })),
+}));
+
+// Mock Firebase firestore
+jest.mock('firebase/firestore', () => ({
+    collection: jest.fn(),
+    doc: jest.fn(),
+    setDoc: jest.fn(),
+    getDocs: jest.fn(),
+    getFirestore: jest.fn(),
+}));
 
 describe('App', () => {
     it('renders without crashing for user not authenticated', () => {
@@ -14,7 +31,7 @@ describe('App', () => {
     });
     // add a query param token with a valid wallet address
     it.skip('renders without crashing for user authenticated', () => {
-        window.history.pushState({}, '', '?token=0x1234567890abcdef');
+        window.history.pushState({}, '', '?token=0x1234567890abcdef&userId=123');
         render(<App />);
         expect(screen.getByText(/Your Points/i)).toBeInTheDocument();
     });
